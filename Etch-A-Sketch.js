f5a5f009189724206a6f7c601a1569da32cbd35c
Andrew Lockwood <andrewtlockwood@gmail.com>
2020-11-25 10:49:18 -0500

    Basic Etch a Sketch is working

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,55 +1,42 @@
 
+
 const grid = document.querySelector("#gameBox");
 
-let Columns = 25;
-let Rows = 25;
 
 
-document.getElementById("gameBox").style.gridTemplateColumns = "repeat(" + Columns + ", 1fr)";
-document.getElementById("gameBox").style.gridTemplateRows = "repeat(" + Rows + ", 1fr)";
+let boxStyle = ' height: 25px; width: 28px; background-color: pink;'
 
 
-let boxStyle = 'margin-top: 15px; margin-bottom: 15px; margin-left: 15px; margin-right: 15px; height: 25px; width: 25px; background-color: black;'
+let generateGrid = function(size = 352) {
+  const gameBox = document.getElementById('gameBox');
 
-const box1 = document.createElement('div');
-box1.setAttribute("style", boxStyle);
+  console.log(gameBox);
+  gameBox.classList.add('small')
 
-const box2 = document.createElement('div');
-box2.setAttribute("style", boxStyle);
+  for (i = 0; i < size; i++){
+    const div = document.createElement('div');
+  //  gameBox.classList.add('small')
+    div.setAttribute("style", boxStyle);
+    div.id = 'pixelBox';
+    gameBox.appendChild(div);
+  //  console.log(div);
+  }
+}
 
-const box3 = document.createElement('div');
-box3.setAttribute("style", boxStyle);
+let paintIt = function(){
+  const gridItems = document.querySelectorAll('#pixelBox');
+  [].forEach.call(gridItems, function(item){
+   // console.log(item);
+    item.addEventListener('mouseenter', (e) => {
+      e.target.style.backgroundColor = '#707070';
+    });
+  });
 
-const box4 = document.createElement('div');
-box4.setAttribute("style", boxStyle);
-
-const box5 = document.createElement('div');
-box5.setAttribute("style", boxStyle);
-
-const box6 = document.createElement('div');
-box6.setAttribute("style", boxStyle);
-
-const box7 = document.createElement('div');
-box7.setAttribute("style", boxStyle);
-
-const box8 = document.createElement('div');
-box8.setAttribute("style", boxStyle);
-
-const box9 = document.createElement('div');
-box9.setAttribute("style", boxStyle);
-
-grid.appendChild(box1);
-grid.appendChild(box2);
-grid.appendChild(box3);
-grid.appendChild(box4);
-grid.appendChild(box5);
-grid.appendChild(box6);
-grid.appendChild(box7);
-grid.appendChild(box8);
-grid.appendChild(box9);
+}
 
 
-
+generateGrid();
+paintIt();
 
 
 
