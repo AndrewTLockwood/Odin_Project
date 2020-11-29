1682fbbb3cde56e4ef8cb10e2e5d67c842a1d6d9
Andrew Lockwood <andrewtlockwood@gmail.com>
2020-11-29 15:23:36 -0500

    Working on size button

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -4,19 +4,30 @@ const grid = document.querySelector("#gameBox");
 
 
 
-let boxStyle = ' height: 25px; width: 28px; background-color: #bfbfbf;'
 
+let boxStyleSmall = ' height: 25px; width: 28px; background-color: #bfbfbf;'
+let boxStyleMedium = 'height: 12.5px; width: 14px; background-color: #bfbfbf;'
+let boxStyleLarge = 'height: 6.25px; width: 7px; background-color: #bfbfbf;'
 
-let generateGrid = function(size = 352) {
+let smallBox = 352;
+let mediumBox = 1408;
+let largeBox = 5632;
+
+let currentSize = "m";
+
+let generateGrid = function(size = 1408, sbox = 'medium', styleB = boxStyleMedium) {
   const gameBox = document.getElementById('gameBox');
 
   console.log(gameBox);
-  gameBox.classList.add('small')
+  gameBox.classList.remove("medium");
+  gameBox.classList.remove("small");
+  gameBox.classList.remove("large");
+  gameBox.classList.add(sbox);
 
   for (i = 0; i < size; i++){
     const div = document.createElement('div');
   //  gameBox.classList.add('small')
-    div.setAttribute("style", boxStyle);
+    div.setAttribute("style", styleB);
     div.id = 'pixelBox';
     gameBox.appendChild(div);
   //  console.log(div);
@@ -34,9 +45,32 @@ let paintIt = function(){
 
 }
 
+let reSize = function (){
+  const sizeButton = document.querySelector('#size');
+  sizeButton.addEventListener("click", e => {
+    if (currentSize == "m"){
+      currentSize = "l";
+      generateGrid(largeBox, "large", boxStyleLarge);
+
+    }
+    else if (currentSize == "l"){
+      currentSize = "s";
+      generateGrid(smallBox, "small", boxStyleSmall);
+    }
+    else if (currentSize == "s"){
+      currentSize = "m";
+      generateGrid(mediumBox, "medium", boxStyleMedium);
+    }
+  });
+
+
+}
+
 
 generateGrid();
 paintIt();
+reSize();
+
 
 
 
