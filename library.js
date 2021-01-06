89bed4f4b26ff20658dc7cd0051d77607ed7b4c8
Andrew Lockwood <andrewtlockwood@gmail.com>
2021-01-06 12:44:10 -0500

    Added functionality to Library

--- library.js
+++ library.js
@@ -1,7 +1,48 @@
 // list of books on top
 // create book object
 // add books from menu
-// number of books
+// number of books -- DONE
 // zoom book
 // add book art
 // delete book
+
+
+let bookNumber = 0;
+
+
+const addBooks = function(newBook){
+  //  const newBook = document.getElementById("newBook");
+    newBook.style.display = "block";
+    bookNumber++;
+}
+
+const Use = function(){
+
+    const bookList = document.querySelector('#bookList');
+    bookList.addEventListener("click", e => {
+        console.log(e);
+    });
+
+    const bookAdd = document.querySelector('#bookAdd');
+    bookAdd.addEventListener("click", e => {
+        console.log(e);
+        const newBook = document.getElementById("newBook");
+        addBooks(newBook);
+        if (bookNumber == 1){
+            document.getElementById("bookNum").innerHTML = bookNumber + " Book";
+        }
+        else {
+        document.getElementById("bookNum").innerHTML = bookNumber + " Books";
+        }
+    });
+
+    window.onclick = function(event) {
+        if (event.target == newBook) {
+          newBook.style.display = "none";
+        }
+
+}
+}
+
+
+Use();
