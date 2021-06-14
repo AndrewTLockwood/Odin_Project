9f5ac1f907e37336e71750966e6af0cf0b78b7be
Andrew Lockwood <andrewtlockwood@gmail.com>
2021-06-14 14:43:21 -0400

    Game working, need to add reset and then classes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,40 @@
 
 let boxNum = {};
 let firstPlayer = true;
+let winPlayer = 0;
+let count = 0;
+let winning = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+
+    [1, 5, 9],
+    [3, 5, 7]
+];
+  
+
+let playerOneNums = [];
+let playerTwoNums = [];
 
 
-let playerOneScore = 0;
-let PlayerTwoScore = 0;
+function endGame () {
+    if (count == 9){
+        document.getElementById("score").innerHTML = "Tie Game!";  
+    }
 
+    if (winPlayer == 1){
+        document.getElementById("score").innerHTML = "Player One Wins"; 
+    }
+
+    else if (winPlayer == 2){
+        document.getElementById("score").innerHTML = "Player Two Wins";
+    }
+
+}
 
 function playGame () {
  
@@ -21,22 +50,38 @@ function playGame () {
                 console.log(i);
                 if (firstPlayer == true){
                     document.getElementById("btn" + i).innerHTML += '<div class="IconX">x</div>'; 
+                    playerOneNums.push(i);
+                    for (let i = 0; i< 8; i++){
+                        let checker = winning[i].every(v => playerOneNums.includes(v));
+                          if (checker == true){
+                            console.log("Win", winning[i]);
+                            winPlayer = 1;
+                          }
+                      }
+                    console.log("player One ", playerOneNums);
+                    count = count + 1;
+                    console.log("turn", count);
+                    endGame();
                     firstPlayer = false;
                 }
                 else {
                     document.getElementById("btn" + i).innerHTML += '<div class="IconO">o</div>'; 
+                    playerTwoNums.push(i);
+                    for (let i = 0; i< 8; i++){
+                        let checker = winning[i].every(v => playerTwoNums.includes(v));
+                          if (checker == true){
+                            console.log("Win", winning[i]);
+                            winPlayer = 2;
+                          }
+                      }
+                    console.log("player Two ", playerTwoNums);
+                    count = count + 1;
+                    console.log("turn", count)
+                    endGame();
                     firstPlayer = true;
                 }
                 boxNum[i] = "t";}
         });
-    }
-
-    if (firstPlayer == true){
-        firstPlayer = false;
-    }
-
-    if (firstPlayer == false){
-        firstPlayer = true;
     }
 
 }
